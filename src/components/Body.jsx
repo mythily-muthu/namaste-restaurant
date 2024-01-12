9363e9f3ce4652c71f6d3ce1142b957ee75286f2
mythily-muthu <mythilimuthu18@gmail.com>
2024-01-12 13:30:18 +0530

    Feat: used swiggy api for restaurant cards

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import { Swiggy_URL } from "../constants";
 import { RestaurantsList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
-import Shimmer from "./Shimmer";
-const Body = () => {
-  const [restaurants, setRestaurants] = useState(RestaurantsList);
-  const [filteredRestaurant, setFilteredRestaurant] = useState(RestaurantsList);
-  const [searchText, setsearchText] = useState("");
+// import Shimmer from "./Shimmer";
+import axios from "axios";
 
-  // shimmer
-  return filteredRestaurant.length === 0 || restaurants.length === 0 ? (
-    <Shimmer />
-  ) : (
+const Body = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  // const [restaurants, setRestaurants] = useState(RestaurantsList);
+  // const [filteredRestaurant, setFilteredRestaurant] = useState(RestaurantsList);
+  const [searchText, setsearchText] = useState("");
+  console.log("f", filteredRestaurant);
+  const getAllRestaurants = async () => {
+    let res = await axios.get(Swiggy_URL);
+    console.log(
+      "res lists:",
+      res.data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setRestaurants(
+      res?.data?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    const restaurantsData =
+      res?.data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    if (restaurantsData) {
+      setRestaurants(restaurantsData);
+    } else {
+      console.log("error unable to receive data ");
+    }
+  };
+
+  useEffect(() => {
+    getAllRestaurants();
+  }, []);
+
+  return (
     <div className="bg-bgcol or w-full px-[15%] py-[2%] flex flex-col gap-y-5">
       {/* search restaurants filter */}
       <div className="flex w-[80%] mx-auto relative h-12">
@@ -41,9 +68,9 @@ const Body = () => {
         <button
           onClick={() => {
             let filteredRestaurants = restaurants.filter(
-              (restaurant) => restaurant?.rating > 4.5
+              (restaurant) => restaurant.rating > 4.5
             );
-            setRestaurants(filteredRestaurants);
+            setFilteredRestaurant(filteredRestaurants);
           }}
           className="flex px-4 py-1 rounded-md border bg-orange-600 text-white hover:border-orange-700 hover:tracking-wide uppercase text-sm font-semibold tracking-wide"
         >
@@ -51,8 +78,8 @@ const Body = () => {
         </button>
       </div>
       <div className="grid grid-cols-4 w-full gap-10 py-3">
-        {filteredRestaurant.map((card) => {
-          return <RestaurantCard card={card} />;
+        {restaurants.map((card) => {
+          return <RestaurantCard card={card} key={card.info.id} />;
         })}
       </div>
     </div>
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import star from ".././utils/star.png";
+import {Cloudinary} from "@cloudinary/url-gen";
 const RestaurantCard = ({ card }) => {
+  console.log("imageurl:", card.info.cloudinaryImageId);
   return (
-    <div className="flex flex-col cursor-pointer " key={card.id}>
+    <div className="flex flex-col cursor-pointer " key={card.info.id}>
       <div className="w-[233px] flex gap-x-4 flex-col rounded-2xl overflow-hidden ">
         <div className="w-[233px] h-[142px] ">
           <img
-            src={card.image}
-            alt={card.name}
-            className="w-full object-fill "
+            src={card.info.cloudinaryImageId}
+            alt={card.info.name}
+            className="w-full h-full object-fill "
           />
         </div>
       </div>
       <div className="w-full p-3">
         <h1 className="text-lg font-medium text-gray-700 tracking-wide">
-          {card.name}
+          {card.info.name}
         </h1>
         <div className="flex gap-1 font-medium">
           <img src={star} alt="rating" className="w-5 h-5" />
           <h2 className="text-base font-medium text-gray-800 ">
-            {card.rating}
+            {card.info.avgRating}
           </h2>
           <span className="flex items-center font-bold">.</span>
-          <h2>{card.deliveryTime}</h2>
+          <h2>{card.info.sla.slaString}</h2>
         </div>
         <h2 className="flex font-light text-gray-500 text-base tracking-wider">
-          {card.cuisines}
+          {card.info.cuisines}
         </h2>
-        <h2 className="flex text-md font-medium">{card.price}</h2>
+        <h2 className="flex text-md font-medium">{card.info.costForTwo}</h2>
       </div>
     </div>
   );
